39f462e0df1746529d1c3874a88f2095f2ed4beb
SpeckyYT <Specky.fans@gmail.com>
2020-07-01 20:17:17 +0200

    made Game.love less heavy

--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
 @echo off
 del game.love
 del game.love.tmp
-7z a -tzip ".\game.love" "./*" -mx0 -xr!.git
+7z a -tzip ".\game.love" "./*" -mx0 -xr!.git -xr!*.git* -xr!*.bat -xr!*.md -xr!*.txt -xr!*.love -xr!*.zip -xr!*.db -xr!*.rst -xr!*.py
