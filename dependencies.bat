685dfd8c8b30b45123f92a15f59b3a6d1772683f
SpeckyYT <Specky.fans@gmail.com>
2020-07-01 18:38:06 +0200

    test for submodules

--- dependencies.bat
+++ dependencies.bat
@@ -1,10 +1,13 @@
 @echo off
+echo "Moonshine"
+rmdir /Q /S "moonshine"
+git submodule add --force https://github.com/vrld/moonshine.git
 echo "Hump"
 rmdir /Q /S "humb"
-git clone https://github.com/vrld/hump.git
+git submodule add --force https://github.com/vrld/hump.git
 echo "MLib"
 rmdir /Q /S "mlib"
-git clone https://github.com/davisdude/mlib.git
+git submodule add --force https://github.com/davisdude/mlib.git
 echo "Konami"
 rmdir /Q /S "Konami"
-git clone https://github.com/Tjakka5/Konami.git
+git submodule add --force https://github.com/Tjakka5/Konami.git
