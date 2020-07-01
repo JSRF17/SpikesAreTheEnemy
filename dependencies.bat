cdd5ed3a1b6c179c70384d36c087a322ab24c0a0
SpeckyYT <Specky.fans@gmail.com>
2020-07-01 20:09:21 +0200

    test4

--- dependencies.bat
+++ dependencies.bat
@@ -1,14 +1,14 @@
 @echo off
 echo "Moonshine"
 rmdir /Q /S "moonshine"
-git submodule add --force https://github.com/SpeckyYT/moonshine.git
+git submodule add -f https://github.com/SpeckyYT/moonshine.git
 echo "Hump"
 rmdir /Q /S "humb"
-git submodule add --force https://github.com/vrld/hump.git
+git submodule add -f https://github.com/vrld/hump.git
 echo "MLib"
 rmdir /Q /S "mlib"
-git submodule add --force https://github.com/davisdude/mlib.git
+git submodule add -f https://github.com/davisdude/mlib.git
 echo "Konami"
 rmdir /Q /S "Konami"
-git submodule add --force https://github.com/Tjakka5/Konami.git
-git submodule update
+git submodule add -f https://github.com/Tjakka5/Konami.git
+git submodule update --remote --merge
